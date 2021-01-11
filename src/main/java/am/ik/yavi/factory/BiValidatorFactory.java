feacd0a1b8a34793ff443cae5b640a93b9c4aeb4
Toshiaki Maki <tmaki@pivotal.io>
2021-01-11 15:11:25 +0900

    Format

--- src/main/java/am/ik/yavi/factory/BiValidatorFactory.java
+++ src/main/java/am/ik/yavi/factory/BiValidatorFactory.java
@@ -38,6 +38,7 @@ import am.ik.yavi.message.MessageFormatter;
  * </pre>
  *
  * A component can create a validator like following:
+ * 
  * <pre>
  *{@literal @RestController}
  * public calls OrderController {
