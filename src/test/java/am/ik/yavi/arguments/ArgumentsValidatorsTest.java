d777bf7e6034c4a1271603333d6f6af24c7137fe
Toshiaki Maki <tmaki@vmware.com>
2021-06-04 00:52:15 +0900

    Add testcases gh-132

--- src/test/java/am/ik/yavi/arguments/ArgumentsValidatorsTest.java
+++ src/test/java/am/ik/yavi/arguments/ArgumentsValidatorsTest.java
@@ -1,7 +1,9 @@
 package am.ik.yavi.arguments;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Stream;
 
 import am.ik.yavi.Address;
@@ -27,6 +29,15 @@ class ArgumentsValidatorsTest {
 			.of("phoneNumber",
 					c -> c.notBlank().greaterThanOrEqual(8).lessThanOrEqual(16))
 			.build(PhoneNumber::new);
+
+	static final Arguments1Validator<Map<String, String>, Country> mapCountryValidator = countryValidator
+			.compose(map -> map.get("country"));
+
+	static final Arguments1Validator<Map<String, String>, String> mapStreetValidator = streetValidator
+			.compose(map -> map.get("street"));
+
+	static final Arguments1Validator<Map<String, String>, PhoneNumber> mapPhoneNumberValidator = phoneNumberValidator
+			.compose(map -> map.get("phoneNumber"));
 
 	static final StringValidator<String> v1 = StringValidatorBuilder
 			.of("s1", c -> c.notBlank()).build();
@@ -62,9 +73,45 @@ class ArgumentsValidatorsTest {
 			.split(v2).split(v3).split(v4).split(v5).split(v6).split(v7).split(v8)
 			.split(v9).split(v10).apply(Arrays::asList);
 
+	final Arguments1Validator<Map<String, String>, String> mapV1 = v1
+			.compose(map -> map.get("s1"));
+
+	final Arguments1Validator<Map<String, String>, String> mapV2 = v2
+			.compose(map -> map.get("s2"));
+
+	final Arguments1Validator<Map<String, String>, String> mapV3 = v3
+			.compose(map -> map.get("s3"));
+
+	final Arguments1Validator<Map<String, String>, String> mapV4 = v4
+			.compose(map -> map.get("s4"));
+
+	final Arguments1Validator<Map<String, String>, String> mapV5 = v5
+			.compose(map -> map.get("s5"));
+
+	final Arguments1Validator<Map<String, String>, String> mapV6 = v6
+			.compose(map -> map.get("s6"));
+
+	final Arguments1Validator<Map<String, String>, String> mapV7 = v7
+			.compose(map -> map.get("s7"));
+
+	final Arguments1Validator<Map<String, String>, String> mapV8 = v8
+			.compose(map -> map.get("s8"));
+
+	final Arguments1Validator<Map<String, String>, String> mapV9 = v9
+			.compose(map -> map.get("s9"));
+
+	final Arguments1Validator<Map<String, String>, String> mapV10 = v10
+			.compose(map -> map.get("s10"));
+
+	final Arguments1Validator<Map<String, String>, List<String>> map10V = mapV1
+			.combine(mapV2).combine(mapV3).combine(mapV4).combine(mapV5).combine(mapV6)
+			.combine(mapV7).combine(mapV8).combine(mapV9).combine(mapV10)
+			.apply(Arrays::asList);
+
 	@ParameterizedTest
-	@MethodSource("validators")
-	void applyValid(Arguments3Validator<String, String, String, Address> validator) {
+	@MethodSource("splitValidators")
+	void splitValid(Arguments3Validator<String, String, String, Address> validator) {
+
 		final Validated<Address> validated = validator.validate("JP", "XYZ Avenue",
 				"123-456-789");
 		assertThat(validated.isValid()).isTrue();
@@ -75,8 +122,8 @@ class ArgumentsValidatorsTest {
 	}
 
 	@ParameterizedTest
-	@MethodSource("validators")
-	void applyInvalid(Arguments3Validator<String, String, String, Address> validator) {
+	@MethodSource("splitValidators")
+	void splitInvalid(Arguments3Validator<String, String, String, Address> validator) {
 		final Validated<Address> validated = validator.validate("J", " ", "1234567");
 		assertThat(validated.isValid()).isFalse();
 		final ConstraintViolations violations = validated.errors();
@@ -92,7 +139,7 @@ class ArgumentsValidatorsTest {
 	}
 
 	@Test
-	void arguments10Valid() {
+	void split10Valid() {
 		final Validated<List<String>> validated = arguments10Validator.validate("1", "2",
 				"3", "4", "5", "6", "7", "8", "9", "10");
 		assertThat(validated.isValid()).isTrue();
@@ -102,16 +149,109 @@ class ArgumentsValidatorsTest {
 	}
 
 	@Test
-	void arguments10Invalid() {
-
+	void split10Invalid() {
+		final Validated<List<String>> validated = arguments10Validator.validate(" ", " ",
+				" ", " ", " ", " ", " ", " ", " ", " ");
+		assertThat(validated.isValid()).isFalse();
+		final ConstraintViolations violations = validated.errors();
+		assertThat(violations).hasSize(10);
+		for (int i = 0; i < 10; i++) {
+			assertThat(violations.get(i).name()).isEqualTo("s" + (i + 1));
+			assertThat(violations.get(i).messageKey()).isEqualTo("charSequence.notBlank");
+		}
 	}
 
-	static Stream<Arguments3Validator<String, String, String, Address>> validators() {
+	@ParameterizedTest
+	@MethodSource("combineValidators")
+	void combineValid(Arguments1Validator<Map<String, String>, Address> validator) {
+		final Validated<Address> validated = validator
+				.validate(new HashMap<String, String>() {
+					{
+						put("country", "JP");
+						put("street", "XYZ Avenue");
+						put("phoneNumber", "123-456-789");
+					}
+				});
+		assertThat(validated.isValid()).isTrue();
+		final Address address = validated.value();
+		assertThat(address.country().name()).isEqualTo("JP");
+		assertThat(address.street()).isEqualTo("XYZ Avenue");
+		assertThat(address.phoneNumber().value()).isEqualTo("123-456-789");
+	}
+
+	@ParameterizedTest
+	@MethodSource("combineValidators")
+	void combineInvalid(Arguments1Validator<Map<String, String>, Address> validator) {
+		final Validated<Address> validated = validator
+				.validate(new HashMap<String, String>() {
+					{
+						put("country", "J");
+						put("street", " ");
+						put("phoneNumber", "1234567");
+					}
+				});
+		assertThat(validated.isValid()).isFalse();
+		final ConstraintViolations violations = validated.errors();
+		assertThat(violations).hasSize(3);
+		assertThat(violations.get(0).name()).isEqualTo("country");
+		assertThat(violations.get(0).messageKey())
+				.isEqualTo("container.greaterThanOrEqual");
+		assertThat(violations.get(1).name()).isEqualTo("street");
+		assertThat(violations.get(1).messageKey()).isEqualTo("charSequence.notBlank");
+		assertThat(violations.get(2).name()).isEqualTo("phoneNumber");
+		assertThat(violations.get(2).messageKey())
+				.isEqualTo("container.greaterThanOrEqual");
+	}
+
+	@Test
+	void combine10Valid() {
+		final Validated<List<String>> validated = map10V
+				.validate(new HashMap<String, String>() {
+					{
+						for (int i = 1; i <= 10; i++) {
+							put("s" + i, String.valueOf(i));
+						}
+					}
+				});
+		assertThat(validated.isValid()).isTrue();
+		final List<String> list = validated.value();
+		assertThat(list).containsExactly("1", "2", "3", "4", "5", "6", "7", "8", "9",
+				"10");
+	}
+
+	@Test
+	void combine10Invalid() {
+		final Validated<List<String>> validated = map10V
+				.validate(new HashMap<String, String>() {
+					{
+						for (int i = 1; i <= 10; i++) {
+							put("s" + i, " ");
+						}
+					}
+				});
+		assertThat(validated.isValid()).isFalse();
+		final ConstraintViolations violations = validated.errors();
+		assertThat(violations).hasSize(10);
+		for (int i = 0; i < 10; i++) {
+			assertThat(violations.get(i).name()).isEqualTo("s" + (i + 1));
+			assertThat(violations.get(i).messageKey()).isEqualTo("charSequence.notBlank");
+		}
+	}
+
+	static Stream<Arguments3Validator<String, String, String, Address>> splitValidators() {
 		return Stream.of(
 				ArgumentsValidators
 						.split(countryValidator, streetValidator, phoneNumberValidator)
 						.apply(Address::new),
 				countryValidator.split(streetValidator).split(phoneNumberValidator)
 						.apply(Address::new));
+	}
+
+	static Stream<Arguments1Validator<Map<String, String>, Address>> combineValidators() {
+		return Stream.of(
+				ArgumentsValidators.combine(mapCountryValidator, mapStreetValidator,
+						mapPhoneNumberValidator).apply(Address::new),
+				mapCountryValidator.combine(mapStreetValidator)
+						.combine(mapPhoneNumberValidator).apply(Address::new));
 	}
 }
