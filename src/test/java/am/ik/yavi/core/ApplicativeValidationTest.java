30de268f6ba1424e750d7e49a41761fcc07ea255
Toshiaki Maki <tmaki@vmware.com>
2021-06-09 00:51:46 +0900

    Add tests to compose/andThen

--- src/test/java/am/ik/yavi/core/ApplicativeValidationTest.java
+++ src/test/java/am/ik/yavi/core/ApplicativeValidationTest.java
@@ -15,7 +15,9 @@
  */
 package am.ik.yavi.core;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Stream;
 
 import am.ik.yavi.Address;
@@ -23,6 +25,7 @@ import am.ik.yavi.Country;
 import am.ik.yavi.PhoneNumber;
 import am.ik.yavi.builder.ValidatorBuilder;
 import am.ik.yavi.fn.Validations;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -35,6 +38,13 @@ class ApplicativeValidationTest {
 	static final ApplicativeValidator<String> streetValidator = ValidatorBuilder
 			.of(String.class)._string(s -> s, "street", c -> c.notBlank()).build()
 			.applicative();
+
+	static final ValueValidator<Map<String, String>, Country> mapCountryValidator = Country
+			.validator().prefixed("country").applicative()
+			.compose(map -> new Country(map.get("country")));
+
+	static final ValueValidator<Country, String> countryValidator = Country.validator()
+			.prefixed("country").applicative().andThen(Country::name);
 
 	@ParameterizedTest
 	@MethodSource("validValidations")
@@ -61,6 +71,46 @@ class ApplicativeValidationTest {
 		assertThat(violations.get(2).name()).isEqualTo("phoneNumber.value");
 		assertThat(violations.get(2).messageKey())
 				.isEqualTo("container.greaterThanOrEqual");
+	}
+
+	@Test
+	void compose_valid() {
+		final Validated<Country> countryValidated = mapCountryValidator
+				.validate(Collections.singletonMap("country", "JP"));
+		assertThat(countryValidated.isValid()).isTrue();
+		assertThat(countryValidated.value().name()).isEqualTo("JP");
+	}
+
+	@Test
+	void compose_invalid() {
+		final Validated<Country> countryValidated = mapCountryValidator
+				.validate(Collections.singletonMap("country", "J"));
+		assertThat(countryValidated.isValid()).isFalse();
+		final ConstraintViolations violations = countryValidated.errors();
+		assertThat(violations.size()).isEqualTo(1);
+		assertThat(violations.get(0).messageKey())
+				.isEqualTo("container.greaterThanOrEqual");
+		assertThat(violations.get(0).name()).isEqualTo("country.name");
+	}
+
+	@Test
+	void andThen_valid() {
+		final Validated<String> countryValidated = countryValidator
+				.validate(new Country("JP"));
+		assertThat(countryValidated.isValid()).isTrue();
+		assertThat(countryValidated.value()).isEqualTo("JP");
+	}
+
+	@Test
+	void andThen_invalid() {
+		final Validated<String> countryValidated = countryValidator
+				.validate(new Country("J"));
+		assertThat(countryValidated.isValid()).isFalse();
+		final ConstraintViolations violations = countryValidated.errors();
+		assertThat(violations.size()).isEqualTo(1);
+		assertThat(violations.get(0).messageKey())
+				.isEqualTo("container.greaterThanOrEqual");
+		assertThat(violations.get(0).name()).isEqualTo("country.name");
 	}
 
 	static Stream<Arguments> validValidations() {
