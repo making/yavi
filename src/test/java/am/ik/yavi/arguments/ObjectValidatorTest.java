7d1e57b7907e808d3e3c13895184dceaa2c37427
Lonre Wang <lonrevip@gmail.com>
2021-06-10 13:48:25 +0800

    Update ObjectValidatorTest.java

--- src/test/java/am/ik/yavi/arguments/ObjectValidatorTest.java
+++ src/test/java/am/ik/yavi/arguments/ObjectValidatorTest.java
@@ -54,19 +54,19 @@ class ObjectValidatorTest {
 	@ParameterizedTest
 	@MethodSource("validators")
 	void validateValid(ObjectValidator<Instant, Date> dateValidator) {
-		final Validated<Date> dataValidated = dateValidator
+		final Validated<Date> dateValidated = dateValidator
 				.validate(Instant.ofEpochMilli(1000L));
-		assertThat(dataValidated.isValid()).isTrue();
-		assertThat(dataValidated.value().getTime()).isEqualTo(1000L);
+		assertThat(dateValidated.isValid()).isTrue();
+		assertThat(dateValidated.value().getTime()).isEqualTo(1000L);
 	}
 
 	@ParameterizedTest
 	@MethodSource("validators")
 	void validateInvalid(ObjectValidator<Instant, Date> dateValidator) {
-		final Validated<Date> dataValidated = dateValidator
+		final Validated<Date> dateValidated = dateValidator
 				.validate(Instant.now().plusSeconds(1));
-		assertThat(dataValidated.isValid()).isFalse();
-		final ConstraintViolations violations = dataValidated.errors();
+		assertThat(dateValidated.isValid()).isFalse();
+		final ConstraintViolations violations = dateValidated.errors();
 		assertThat(violations).hasSize(1);
 		assertThat(violations.get(0).name()).isEqualTo("createdAt");
 		assertThat(violations.get(0).messageKey()).isEqualTo("instant.past");
@@ -94,9 +94,9 @@ class ObjectValidatorTest {
 				Instant.ofEpochMilli(1000L));
 		final Arguments1Validator<Map<String, Instant>, Date> mapValidator = dateValidator
 				.compose(map -> map.get("createdAt"));
-		final Validated<Date> dataValidated = mapValidator.validate(params);
-		assertThat(dataValidated.isValid()).isTrue();
-		assertThat(dataValidated.value().getTime()).isEqualTo(1000L);
+		final Validated<Date> dateValidated = mapValidator.validate(params);
+		assertThat(dateValidated.isValid()).isTrue();
+		assertThat(dateValidated.value().getTime()).isEqualTo(1000L);
 	}
 
 	@ParameterizedTest
@@ -106,9 +106,9 @@ class ObjectValidatorTest {
 				Instant.now().plusSeconds(1));
 		final Arguments1Validator<Map<String, Instant>, Date> mapValidator = dateValidator
 				.compose(map -> map.get("createdAt"));
-		final Validated<Date> dataValidated = mapValidator.validate(params);
-		assertThat(dataValidated.isValid()).isFalse();
-		final ConstraintViolations violations = dataValidated.errors();
+		final Validated<Date> dateValidated = mapValidator.validate(params);
+		assertThat(dateValidated.isValid()).isFalse();
+		final ConstraintViolations violations = dateValidated.errors();
 		assertThat(violations).hasSize(1);
 		assertThat(violations.get(0).name()).isEqualTo("createdAt");
 		assertThat(violations.get(0).messageKey()).isEqualTo("instant.past");
