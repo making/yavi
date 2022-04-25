67e6e21613b152fc5aa887f229b4a7dc113085ee
Toshiaki Maki <tmaki@vmware.com>
2022-04-25 20:18:59 +0900

    Make memoization configurable in TemporalConstraintBase fixes gh-229

---
+++ src/test/java/am/ik/yavi/constraint/Gh229Test.java
@@ -0,0 +1,39 @@
+package am.ik.yavi.constraint;
+
+import java.time.Instant;
+import java.util.function.Supplier;
+
+import am.ik.yavi.builder.ValidatorBuilder;
+import am.ik.yavi.core.ConstraintViolations;
+import am.ik.yavi.core.Validator;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class Gh229Test {
+	@Test
+	void validationResultShouldDifferIfMemoizeIsTrue() {
+		final Validator<Supplier<Instant>> validator = ValidatorBuilder
+				.<Supplier<Instant>> of()
+				._instant(Supplier::get, "date", c -> c.before(Instant::now, true))
+				.build();
+		final ConstraintViolations violations1 = validator.validate(Instant::now);
+		assertThat(violations1.isValid()).isTrue();
+		final ConstraintViolations violations2 = validator.validate(Instant::now);
+		assertThat(violations2.isValid()).isFalse();
+		assertThat(violations2.size()).isEqualTo(1);
+		assertThat(violations2.get(0).messageKey()).isEqualTo("temporal.before");
+	}
+
+	@Test
+	void validationResultShouldNotDifferIfMemoizeIsFalse() {
+		final Validator<Supplier<Instant>> validator = ValidatorBuilder
+				.<Supplier<Instant>> of()
+				._instant(Supplier::get, "date", c -> c.before(Instant::now, false))
+				.build();
+		final ConstraintViolations violations1 = validator.validate(Instant::now);
+		assertThat(violations1.isValid()).isTrue();
+		final ConstraintViolations violations2 = validator.validate(Instant::now);
+		assertThat(violations2.isValid()).isTrue();
+	}
+}
